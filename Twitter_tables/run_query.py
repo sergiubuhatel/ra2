045c8660fa2e3126ee9ad7876c40ab5ace4e5f61
Sergiu Buhatel <sergiu.buhatel@gmail.com>
2025-03-22 16:41:33 -0400

    Update run_query.py

--- Twitter_tables/run_query.py
+++ Twitter_tables/run_query.py
@@ -17,7 +17,14 @@ end_time = time.time()
 
 print(f"Time taken to load the file: {end_time - start_time} seconds")
 
-query = f"SELECT \"AFL\", screen_name4, count(*) as numConnections FROM {args.table} " \
-        f"WHERE screen_name4!= 'AFL' AND tweet_text REGEXP '\\$\\bAFL\\b' group by screen_name4"
+query = f"SELECT 'AFL' AS AFL, screen_name4, COUNT(*) AS numConnections" \
+f" FROM {args.table}" \
+f" WHERE screen_name4 <> 'AFL'" \
+f" AND tweet_text LIKE '%AFL%'" \
+f" GROUP BY screen_name4"
+
+
+#query = f"SELECT \"AFL\", screen_name4, count(*) as numConnections FROM {args.table} " \
+#        f"WHERE screen_name4!= 'AFL' AND tweet_text REGEXP '\\$\\bAFL\\$' group by screen_name4"
 result = bc.sql(query)
 print(result)
