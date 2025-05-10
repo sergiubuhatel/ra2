e63a1401c8388ac3ef3b7aced93b00f8acf5cb7d
Sergiu Buhatel <sergiu.buhatel@gmail.com>
2025-05-10 11:30:11 -0400

    Documentation

--- OmniSci/launch.sh
+++ OmniSci/launch.sh
@@ -1,7 +1,6 @@
 sudo -E docker run --runtime=nvidia \
   --name omnisci \
   -v /home/ra2/omnisci/omnisci-docker-storage:/omnisci-storage \
-  -v /home/ra2:/mnt/data \
-  -v /home/ra2/omnisci/server.conf:/omnisci/config/server.conf \
+  -v /home/ra2:/data \
   -p 6273-6280:6273-6280 \
-   omnisci/core-os-cuda:latest
+  omnisci/core-os-cuda:latest
