0ed4875f5afaccb467b97dfdcfaf9558040e5dfb
Sergiu Buhatel <sergiu.buhatel@gmail.com>
2025-07-12 11:13:29 -0400

    Added connected firms

--- graph/frontend/src/components/NodeInfoPanel.js
+++ graph/frontend/src/components/NodeInfoPanel.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { useSelector } from "react-redux";
 import IconButton from "@mui/material/IconButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
 export default function NodeInfoPanel({ node, onClose }) {
+  const [connections, setConnections] = useState([]);
+  const fileContent = useSelector((state) => state.file.content);
+
+  const getSortedUniqueTargetsBySource = (sourceTicker, edges) => {
+    const seen = new Set();
+
+    return edges
+      .filter(edge => edge.source === sourceTicker)
+      .sort((a, b) => b.weight - a.weight)
+      .filter(edge => {
+        if (seen.has(edge.target)) return false;
+        seen.add(edge.target);
+        return true;
+      })
+      .map(edge => edge.target);
+  };
+
+  useEffect(() => {
+    if (fileContent) {
+      console.log("File content has changed:", fileContent);
+    }
+  }, [fileContent]);
+
+  useEffect(() => {
+    setConnections(getSortedUniqueTargetsBySource(node.label, fileContent.edges));
+  }, [node]);
+
   return (
     <div
       style={{
@@ -75,8 +103,13 @@ export default function NodeInfoPanel({ node, onClose }) {
             <strong>Connections:</strong> 
           </p>
           <p style={{ marginLeft: '5px', fontSize: '0.85em' }}>
-            <strong>Connected Firms ()</strong> 
+            <strong>Connected Firms ({connections.length})</strong>
           </p>
+          <ul style={{ marginLeft: '20px', fontSize: '0.85em' }}>
+            {connections.map((ticker) => (
+              <li key={ticker}>{ticker}</li>
+            ))}
+          </ul>
         </div>
       ) : (
         <p>Click a node to view details</p>
