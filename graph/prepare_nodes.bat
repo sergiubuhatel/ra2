43b6b2d713dc643f9347fa49d94676facf94001c
Sergiu Buhatel <sergiu.buhatel@gmail.com>
2025-07-26 16:02:23 -0400

    Bat script

---
+++ graph/prepare_nodes.bat
@@ -0,0 +1,30 @@
+@echo off
+REM Usage: run_prepare_nodes.bat [YEAR]
+
+SETLOCAL
+
+REM Check if a year is provided
+IF "%~1"=="" (
+    echo Usage: %0 [YEAR]
+    exit /b 1
+)
+
+SET YEAR=%~1
+
+REM Run the Python script with year-based paths
+python prepare_nodes.py ^
+  --industries ./frontend/public/data/%YEAR%/%YEAR%_industries.csv ^
+  --top ./frontend/public/data/%YEAR%/Top50_%YEAR%.csv ^
+  --output ./frontend/public/data/%YEAR%/Top50_tickers.csv
+
+python prepare_nodes.py ^
+  --industries ./frontend/public/data/%YEAR%/%YEAR%_industries.csv ^
+  --top ./frontend/public/data/%YEAR%/Top100_%YEAR%.csv ^
+  --output ./frontend/public/data/%YEAR%/Top100_tickers.csv
+
+python prepare_nodes.py ^
+  --industries ./frontend/public/data/%YEAR%/%YEAR%_industries.csv ^
+  --top ./frontend/public/data/%YEAR%/Top200_%YEAR%.csv ^
+  --output ./frontend/public/data/%YEAR%/Top200_tickers.csv    
+
+ENDLOCAL
