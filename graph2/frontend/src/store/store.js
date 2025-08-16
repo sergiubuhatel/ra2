2bcda19d6007214ab6194812472e952c2c160e0b
Sergiu Buhatel <sergiu.buhatel@gmail.com>
2025-08-16 14:00:07 -0400

    remove unnecesary project

--- graph2/frontend/src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import fileReducer from "./fileSlice"; // adjust path as needed
-
-export const store = configureStore({
-  reducer: {
-    file: fileReducer,
-    // other reducers...
-  },
-});
